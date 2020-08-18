00d7ecce57d58617d43e0f604ce969f997a951a1
user <anderson.cancado@aromni.com>
2020-08-18 21:13:01 +0100

    added initial files

---
+++ proc_video.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import time
+import subprocess as sp
+from os import remove
+from tqdm import tqdm
+import argparse
+import pathlib
+
+def process_video(file_name, output_file_name):
+    cap = cv.VideoCapture(file_name)
+
+    width, height = (
+            int(cap.get(cv.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+        )
+    fps = int(cap.get(cv.CAP_PROP_FPS))
+    total_frames = cap.get(7)
+
+    fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+    out = cv.VideoWriter()
+    out.open(output_file_name, fourcc, fps, (width, height), True)
+
+    try:
+        with tqdm(total=total_frames) as pbar:
+            while cap.isOpened():
+                pbar.update(1)
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                im = frame
+                # Example process image
+                grey = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+                ########################################################################
+
+                #make inference here & save in out.write(frm)
+
+
+                ########################################################################
+                out.write(im)
+    finally:
+        # Release resources
+        cap.release()
+        out.release()
+
+    
+
+def single_process(videofile_name, output_file_name):
+    start_time = time.time()
+    process_video(videofile_name, output_file_name)
+    end_time = time.time()
+    total_processing_time = end_time - start_time
+    print("Time taken: {} seconds".format(total_processing_time))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_video", help="input file path")
+    parser.add_argument("output_file_name", help="output file name")
+    args = parser.parse_args()  
+
+    fl = pathlib.Path(args.input_video)
+    if not fl.exists:
+        print("Input path not valid")
+        exit(1)
+
+    videofile_name = args.input_video
+    output_file_name = args.output_file_name
+
+    single_process(videofile_name, output_file_name)
+
+if __name__  == "__main__":
+    main() 
